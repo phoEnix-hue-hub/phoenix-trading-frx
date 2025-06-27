24fcee806e3f9a50fc489f98575f64bc71424895
oluwafemi <erota.oluwafemi@gmail.com>
2025-06-27 15:50:47 +0100

    Remove api directory from phoenix-trading-frx repo

--- api/src/trades/trades.module.ts
+++
@@ -1,8 +0,0 @@
-// src/trades/trades.module.ts
-import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module'; // Import AuthModule
-
-@Module({
-  imports: [AuthModule], // Include AuthModule to provide AuthService
-})
-export class TradesModule {}
